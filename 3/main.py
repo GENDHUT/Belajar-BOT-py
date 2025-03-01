a6988ace0bbcd040f087307528f25874736e1fdb
GENDHUT <Kecikcemrincingan15@gmail.com>
2025-03-01 08:45:31 +0700

    tutor 3

--- 3/main.py
+++ 3/main.py
@@ -3,49 +3,20 @@ import telebot
 
 bot = telebot.TeleBot(token=API_TOKEN)
 
-# @bot.message_handler(commands=['start'])
-# def welcome(message):
-#     welcome_text = f'user {message.from_user.first_name} Test berhasil di jalankan'
-#     bot.send_message(message.chat.id, welcome_text)
-    
-# @bot.message_handler(func=lambda message: True)
-# def reply_func(message):
-#     bot.reply_to(message, text="Ada yang bisa saya bantu?")
-
-# --- bagian sesi 2
-
-# @bot.message_handler(content_types=['audio','document'])
-# def handle_audio_doc(massege):
-#     if massege.audio:
-#         bot.reply_to(massege, "ini adalah audio file")
-#     elif massege.document:
-#         bot.reply_to(massege, "ini adalah document file")
-#  -> ini adalah pengecekan audio dan document  berdasarkan kiriman file dari user
-
-@bot.message_handler(regexp="2025")
-def handle_massege(massege):
-    bot.reply_to(massege, "Chat anda mengandung '2025'.")
-# -> pengecekan per chat bila mengandung data regexp dari chat user
-
-# @bot.message_handler(func=lambda m: m.document.mime_type == 'text/plain', content_types=['document'])
-# def handle_text_doc(massege):
-#     bot.reply_to(massege, "Ini adalah text file") 
-# -> pengecekan type file dengan filter
-
-def test_massege(massege):
-    return massege.document.mime_type == 'text/plain'
-
-@bot.message_handler(func=test_massege, content_types=['document'])
-def handle_text_doc(massege):
-    bot.reply_to(massege, "Ini adalah text file")
-# -> mengambil func dari luar handler 
+user_id = []
 
 @bot.message_handler(commands=['start'])
-@bot.message_handler(func=lambda m:m.text == '😂')
-def kirim_apa(message):
-    bot.reply_to(message, "Emoji Dan Start")
-# -> func OR atau pengecekan 2 func
+def welcome(message):
+    bot.send_message(message.chat.id, "welcome")
+    if message.chat.id not in user_id:
+        user_id.append(message.chat_id)
+# -> pengecekan apakah user pernah chat dengan bot atau tidak jika ya lanjut jika tidak append user tersbut
 
+@bot.message_handler(commands=['update'])
+def send_update(massage):
+    for id in user_id:
+        bot.send_message(id, "A new product tersedia")
+# -> bila user ada di dafta user_id tampilkan send_message
 
     
 bot.polling()
