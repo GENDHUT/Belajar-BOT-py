f37e04a2017108310986b9caca60adf16a510743
GENDHUT <Kecikcemrincingan15@gmail.com>
2025-03-03 21:31:36 +0700

    atur bot group (DONE)

--- 7/main.py
+++ 7/main.py
@@ -1,47 +1,41 @@
 from config import API_TOKEN
 from telebot import TeleBot
-import sqlite3
-from telebot.types import KeyboardButton,ReplyKeyboardMarkup
 
 bot = TeleBot(API_TOKEN)
 
-# keyboard BTN
-keyboard = ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
-button = KeyboardButton(text="INFO SAYA!",request_contact=True)
-keyboard.add(button)
-
-# DB
-with sqlite3.connect('user.db') as connection:
-    cursor = connection.cursor()
-    create_table_query= """
-    CREATE TABLE IF NOT EXISTS users(
-        id integer primary key,
-        first_name text,
-        last_name text,
-        phone_number
-    );
-    """
-    cursor. execute(create_table_query)
-
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.send_message(message.chat.id, text="Hai ada yang bisa saya bantu?", reply_markup=keyboard)
+    bot.send_message(message.chat.id, "welcome ada yang bisa saya bantu?")
     
-@bot.message_handler(content_types=['contact'])
-def contact(message):
-    # bot.send_message(message.chat.id, text=f'{message.contact}')
-    with sqlite3.connect('user.db') as connection:
-        cursor = connection.cursor()
-        insert_data_query = """
-        INSERT INTO users( id, first_name, last_name, phone_number)
-        VALUES (?,?,?,?)
-        """
-        data = (
-            message.contact.user_id,
-            message.contact.first_name,
-            message.contact.last_name,
-            message.contact.phone_number
-        )
-        cursor.execute(insert_data_query,data)
+@bot.message_handler(content_types=['new_chat_members'])
+def welcome_new_member(message):
+    for new_member in message.new_chat_member:
+        welcome_text = f'😍 {message.from_user.fist_name}, Selamat datang !!!'
+        bot.send_message(message.chat.id, text=welcome_text)
+        
+def is_user_admin(chat_id,user_id):
+    admins = bot.get_chat_administrators(chat_id)
+    for admins in admins:
+        if admins.user.id == user_id:
+            return True
+    return False
+
+@bot.message_handler(func=lambda message: message.text == "pin")
+def pin_massage(massage):
+    chat_id = massage.chat.id
+    user_id = massage.from_user.id
+    
+    if is_user_admin(chat_id,user_id):
+        if massage.reply_to_message:
+            bot.pin_chat_message(chat_id, massage.reply_to_message.message_id)
+            bot.reply_to(massage.reply_to_message, "Message berhasil di pin!")
+        else:
+            bot.reply_to(massage.reply_to_message, "pilih massage anda sebelum pin!")
+    else:
+        bot.reply_to(massage.chat.id, "Hanya admin yang dapat membuat pin massage!")
+
+
+            
+    
 
 bot.polling()
